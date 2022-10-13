20ed5042c42ed8c96bd19d86a1d0716d1369f597
Alex <alexrobertomata@hotmail.com>
2022-10-13 14:59:06 -0600

    Se modificó la verificación de credenciales verificando una contraseña encriptada

--- src/main/java/com/example/demo/dao/UsuarioDaoImp.java
+++ src/main/java/com/example/demo/dao/UsuarioDaoImp.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,12 +39,36 @@ public class UsuarioDaoImp implements UsuarioDao{
 
     @Override
     public boolean verificarCredenciales(Usuario usuario){
+        String query = "FROM Usuario WHERE email = :email";
+        List<Usuario> lista = entityManager.createQuery(query)
+                .setParameter("email", usuario.getEmail())
+                .getResultList();
+
+        //Gets the password
+        String hashedPassword = lista.get(0).getPassword();
+
+        //TO VERIFY THE PASSWORD
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        /*Compares a Hash with a password. returns a boolean*/
+        //Para guardar en una variable
+        //boolean passwordEsLaMisma = argon2.verify(hashedPassword, usuario.getPassword());
+
+        if(lista.isEmpty()){
+            return false;
+        }
+
+        return argon2.verify(hashedPassword, usuario.getPassword());
+
+        /* ***PORTION BELOW DOES ALMOST THE SAME BUT WITH NO ENCRYPTION AND LESS OPTIMIZED*** */
+
+        /* This portion of code verifies if the email and user are the same of the DB with an unencrypted password
         String query = "FROM Usuario WHERE email = :email AND password = :password";
         List<Usuario> lista = entityManager.createQuery(query)
                 .setParameter("email", usuario.getEmail())
                 .setParameter("password", usuario.getPassword())
                 .getResultList();
-        /*
+         */
+        /* This portion of code is to return if the list is empty
         if(lista.isEmpty()){
             return false;
         }
@@ -50,7 +76,6 @@ public class UsuarioDaoImp implements UsuarioDao{
             return true;
         }
         */
-        return !lista.isEmpty();
     }
 
     @Override
