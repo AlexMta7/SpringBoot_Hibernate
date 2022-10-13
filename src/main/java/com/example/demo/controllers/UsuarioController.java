5622e638165cea6177635991c3bad675e0ecc572
Alex <alexrobertomata@hotmail.com>
2022-10-13 12:04:24 -0600

    Se modificó registarUsuario() para que llame al metodo verificarUsuario() en UsuarioDaoImp que verifica si el usuario existe

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import com.example.demo.dao.UsuarioDao;
 import com.example.demo.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,8 +33,25 @@ public class UsuarioController {
     }
 
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
-    public void registrarUsuario(@RequestBody Usuario usuario){
-        UsuarioDao.registrarUsuario(usuario);
+    public String registrarUsuario(@RequestBody Usuario usuario){
+        if(!UsuarioDao.verificarUsuario(usuario)){
+            Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+            // i: iteraciones,i1:cantidad de memoria, i2:paralelismo (procesos simultaneos, mientras más iteraciones
+            // mayor paralelismo para mayor velocidad de procesamiento), usuario.getPassword(): dato a hashear;
+            // mientras más iteraciones más segura es la contraseña
+            String hash = argon2.hash(1,1024,1,usuario.getPassword());
+            //Se establece la contraseña con el hash
+            usuario.setPassword(hash);
+            //modify DB to accept a long string
+
+            UsuarioDao.registrarUsuario(usuario);
+            return "OK";
+        }
+        else {
+            return "FAIL";
+        }
+
+
     }
 
     @RequestMapping(value = "usuario123")
