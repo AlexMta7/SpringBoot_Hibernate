cc61eb708a4d44a47000b5fdabea51808f4e5431
Alex <alexrobertomata@hotmail.com>
2022-10-14 10:17:32 -0600

    Se modificó el metodo de obtener usuarios para que trabaje con JWT y retorne el token si el usuario existe

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.dao.UsuarioDao;
 import com.example.demo.models.Usuario;
+import com.example.demo.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,23 @@ public class AuthController {
 
     @Autowired
     private UsuarioDao usuarioDao;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public String login(@RequestBody Usuario usuario){
+
+        Usuario loggedUser = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
+        if (loggedUser != null){
+
+            String tokenJwt = jwtUtil.create(String.valueOf(loggedUser.getId()), loggedUser.getEmail());
+            return tokenJwt;
+        }
+        return "FAIL";
+    }
+
+    /* VERIFICACIÓN DE CREDENCIALES SIN JWT
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
     public String login(@RequestBody Usuario usuario){
        if (usuarioDao.verificarCredenciales(usuario)){
@@ -20,6 +38,7 @@ public class AuthController {
        }
        return "FAIL";
     }
+     */
 
 
 
